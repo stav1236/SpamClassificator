505b276bebdde10aac8ac5e43dda900ddb5f8b3a
stav1236 <stavmaor1@gmail.com>
2024-04-30 00:15:33 +0300

    another way to achieve deletion when using Bloom filter data structure. (Filter Cascades)

---
+++ SpamClassifier.js
@@ -0,0 +1,23 @@
+import { BloomFilter } from "./BloomFilter.js";
+
+//another way to achieve deletion when using Bloom filter data structure.
+export class SpamClassifier {
+  constructor(size) {
+    this.spamList = new BloomFilter(size);
+    this.deletedList = new BloomFilter(size);
+  }
+
+  Add_Spam(address) {
+    this.spamList.Add_Spam(address);
+  }
+
+  Remove_Spam(address) {
+    this.deletedList.Add_Spam(address);
+  }
+
+  Is_Spam(address) {
+    if (this.deletedList.Is_Spam(address)) return false; //is remove from the spam list
+
+    return this.spamList.Is_Spam(address);
+  }
+}
